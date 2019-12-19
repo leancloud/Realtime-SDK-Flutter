c289be2652ed38e1faad5c958843dcd736b56db3
jwfing <jwfing@gmail.com>
2019-12-19 17:26:06 +0800

    boilerplate project

---
+++ ios/Classes/LeancloudPlugin.m
@@ -0,0 +1,15 @@
+#import "LeancloudPlugin.h"
+#if __has_include(<leancloud_plugin/leancloud_plugin-Swift.h>)
+#import <leancloud_plugin/leancloud_plugin-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "leancloud_plugin-Swift.h"
+#endif
+
+@implementation LeancloudPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftLeancloudPlugin registerWithRegistrar:registrar];
+}
+@end
