3e4ef46d5789d25d289db8603ed7fa0d296c6187
zapcannon87 <zapcannon87@outlook.com>
2019-12-30 20:02:32 +0800

    feat(ios): client init & deinit

--- ios/Classes/SwiftLeancloudPlugin.swift
+++ ios/Classes/SwiftLeancloudPlugin.swift
@@ -1,14 +1,58 @@
 import Flutter
 import UIKit
+import LeanCloud
 
 public class SwiftLeancloudPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "leancloud_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftLeancloudPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "leancloud_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftLeancloudPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    var clientMap: [String: IMClientDelegator] = [:]
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let args = call.arguments as! [String: Any]
+        switch call.method {
+        case "initClient":
+            let clientId = args["clientId"] as! String
+            let tag = args["tag"] as? String
+            do {
+                let delegator = try IMClientDelegator(ID: clientId, tag: tag)
+                self.clientMap[clientId] = delegator
+                result([:])
+            } catch {
+                let err = error as! LCError
+                result(["error": ["code": err.code]])
+            }
+        case "deinitClient":
+            let clientId = args["clientId"] as! String
+            self.clientMap.removeValue(forKey: clientId)
+            result([:])
+        case "openClient":
+            break
+        case "closeClient":
+            break
+        default:
+            fatalError("unknown method.");
+        }
+    }
+}
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+class IMClientDelegator: IMClientDelegate {
+    
+    let client: IMClient
+    
+    init(ID: String, tag: String?) throws {
+        self.client = try IMClient(ID: ID, tag: tag)
+        self.client.delegate = self
+    }
+    
+    func client(_ client: IMClient, event: IMClientEvent) {
+        
+    }
+    
+    func client(_ client: IMClient, conversation: IMConversation, event: IMConversationEvent) {
+        
+    }
 }
