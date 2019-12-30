f297719bd9d5bbbb39af97c4cce657935d516b85
zapcannon87 <zapcannon87@outlook.com>
2019-12-30 11:57:19 +0800

    chore(ios): init example project

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,23 @@
 import UIKit
 import Flutter
+import LeanCloud
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        do {
+            LCApplication.logLevel = .all
+            try LCApplication.default.set(
+                id: "heQFQ0SwoQqiI3gEAcvKXjeR-gzGzoHsz",
+                key: "lNSjPPPDohJjYMJcQSxi9qAm",
+                serverURL: "https://heqfq0sw.lc-cn-n1-shared.com")
+            GeneratedPluginRegistrant.register(with: self)
+            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        } catch {
+            fatalError("\(error)")
+        }
+    }
 }
