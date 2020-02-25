3f773d957a106406f5266489f959cddd88788bac
zapcannon87 <zapcannon87@outlook.com>
2020-02-25 11:54:13 +0800

    test: update ios config

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -14,6 +14,10 @@ import LeanCloud
                 id: "heQFQ0SwoQqiI3gEAcvKXjeR-gzGzoHsz",
                 key: "lNSjPPPDohJjYMJcQSxi9qAm",
                 serverURL: "https://heqfq0sw.lc-cn-n1-shared.com")
+//            try LCApplication.default.set(
+//                id: "s0g5kxj7ajtf6n2wt8fqty18p25gmvgrh7b430iuugsde212",
+//                key: "hc7jpfubg5vaurjlezxhfr1t9pqb9w8tfw0puz1g83vl9nwz",
+//                serverURL: "https://s0g5kxj7.lc-cn-n1-shared.com")
             GeneratedPluginRegistrant.register(with: self)
             return super.application(application, didFinishLaunchingWithOptions: launchOptions)
         } catch {
